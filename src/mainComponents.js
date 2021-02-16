85efb67779fb4c30be191a209aebf2e52760c263
Erdem Darcan <erdemdarcan@hotmail.com.tr>
2021-02-16 10:37:28 +0100

    Add test images on playArea

---
+++ src/mainComponents.js
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+export const ImageBase = styled.img({
+  maxWidth: "100%",
+});
--- src/playArea.js
+++ src/playArea.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { ImageBase } from "./mainComponents.js";
 import {
   CardsToPlay,
   SelectedCards,
@@ -11,11 +12,20 @@ import {
   BattleGoal,
 } from "./areaComponents.js";
 
+const mt = require("./gloomhaven/images/character-ability-cards/MT/*.png");
+const items = require("./gloomhaven/images/items/1-14/*.png");
+const bg = require("./gloomhaven/images/battle-goals/battlegoal-back.png");
+
 export function PlayArea() {
   return (
     <>
       <CardsToPlay>
         <center>PlayArea</center>
+        <ImageBase src={mt["mt-back"]} width={175}></ImageBase>
+        <ImageBase src={mt["possession"]} width={175}></ImageBase>
+        <ImageBase src={mt["perverse-edge"]} width={175}></ImageBase>
+        <ImageBase src={mt["fearsome-blade"]} width={175}></ImageBase>
+        <ImageBase src={mt["hostile-takeover"]} width={175}></ImageBase>
       </CardsToPlay>
       <SelectedCards>
         <center>SelectedCards</center>
@@ -34,12 +44,17 @@ export function PlayArea() {
       </CharacterSheet>
       <Items>
         <center>Items</center>
+        <ImageBase src={items["poison-dagger"]} width={150}></ImageBase>
+        <ImageBase src={items["minor-stamina-potion"]} width={150}></ImageBase>
       </Items>
       <ModifierArea>
         <center>ModifierArea</center>
       </ModifierArea>
       <BattleGoal>
-        <center>BattleGoal</center>
+        <center>
+          BattleGoal
+          <ImageBase src={bg} width={150}></ImageBase>
+        </center>
       </BattleGoal>
     </>
   );
